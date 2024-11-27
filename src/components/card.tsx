803d0c38b532768452e92b502c9a979b8cf59c85
mlevitorresv <levitorres66@gmail.com>
2024-11-27 14:50:10 +0100

    añadido componente y su interfaz card, para las tarjetas

---
+++ src/components/card.tsx
@@ -0,0 +1,26 @@
+import { CardInterface } from '@/interfaces/componentsInterface';
+import React from 'react'
+
+export default function Card(props: CardInterface) {
+    return (
+        <div className="relative group w-64 h-80">
+            {/* Imagen de fondo */}
+            <div
+                className="w-full h-full bg-cover bg-center rounded-lg"
+                style={{ backgroundImage: `url(${props.image})` }}
+            ></div>
+
+            {/* Capa de información que aparece al hacer hover */}
+            <div
+                className="absolute inset-0 bg-black bg-opacity-50 text-white p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg"
+            >
+                <p>Name: {props.name}</p>
+                <p>Race: {props.race}</p>
+                <p>Gender: {props.gender}</p>
+                <p>Ki: {props.ki}</p>
+                <p>Max. Ki: {props.maxKi}</p>
+                <p>Affiliation: {props.affiliation}</p>
+            </div>
+        </div>
+    );
+}
--- src/interfaces/componentsInterface.ts
+++ src/interfaces/componentsInterface.ts
@@ -3,3 +3,13 @@
 export interface TitleInterface{
     text: String
 }
+
+export interface CardInterface{
+    image: String,
+    name: String,
+    race: String,
+    gender: String,
+    ki: String,
+    maxKi: String,
+    affiliation: String
+}
