18641109e0fdcad0ab9d1617aedaf90e6d58dee4
mlevitorresv <levitorres66@gmail.com>
2024-11-27 15:28:03 +0100

    llamadaa a API y datos mostrados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import DivCardsContainer from "@/components/divCardsContainer";
 import Header from "@/components/header";
+
+const queryClient = new QueryClient();
+
 export default function Home() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <DivCardsContainer />
-    </>
+    </QueryClientProvider>
+    
   );
 }
--- src/components/divCardsContainer.tsx
+++ src/components/divCardsContainer.tsx
@@ -1,9 +1,40 @@
+'use client'
+
 import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { CardInterface } from '@/interfaces/componentsInterface';
+import Card from './card';
+
+
+async function fetchData() {
+    const response = await fetch('https://dragonball-api.com/api/characters');
+    return response.json();
+}
 
 export default function DivCardsContainer() {
-  return (
-    <div className='w-full h-full flex justify-around p-8 bg-cover bg-center' style={{ backgroundImage: "url('/dragonBalls.png')" }}>
-      
-    </div>
-  )
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['cardsData'],
+        queryFn: fetchData
+    });
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error loading data</div>;
+
+    return (
+        <div className='w-full h-full flex justify-around p-8 bg-cover bg-center' style={{ backgroundImage: "url('/dragonBalls.png')" }}>
+            {data.items.map((character: any, index: number) => (
+                <Card
+                    key={index}
+                    image={character.image}
+                    name={character.name}
+                    race={character.race}
+                    gender={character.gender}
+                    ki={character.ki}
+                    maxKi={character.maxKi}
+                    affiliation={character.affiliation}
+                />
+            ))
+            }
+        </div>
+    )
 }
