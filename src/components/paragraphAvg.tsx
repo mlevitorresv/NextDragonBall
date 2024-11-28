2c7a63dd7fd68326b7fe4f429ec540aded4e61ab
mlevitorresv <levitorres66@gmail.com>
2024-11-28 11:20:21 +0100

    size de texto en parrafo cambiado2

--- src/components/paragraphAvg.tsx
+++ src/components/paragraphAvg.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ParagraphAvg(props: ParagraphAvgInterface) {
   return (
     props.texts.map((text, index) => (
-        <p className='text-black w-3/5 mx-auto my-8' key={index}>
+        <p className='text-black text-lg w-3/5 mx-auto my-8' key={index}>
             {text}
         </p>
     )
