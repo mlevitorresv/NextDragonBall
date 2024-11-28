074ecb78899d3981655e9aacb8240f0055c5e428
mlevitorresv <levitorres66@gmail.com>
2024-11-28 10:39:11 +0100

    añaddio link a header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Title from '@/components/title'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className='w-full flex justify-around bg-black text-white p-8'>
-        <Title text='DRAGON BALL' />
-        <Title text='Personajes' />
-        <Title text='Planetas' />
-        <Title text='MLEVITORRESV' />
+        <Link href={'https://web.dragonball-api.com/'}><Title text='DRAGON BALL' /></Link>
+        <Link href={'/characters'}><Title text='Personajes' /></Link>
+        <Link href={'https://www.linkedin.com/in/manuel-lev%C3%AD-torres-vicente/'}><Title text='MLEVITORRESV' /></Link>
     </header>
   )
 }
