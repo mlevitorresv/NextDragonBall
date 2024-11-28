b728b9802042072880d8b8596b8734e7726b9bdd
mlevitorresv <levitorres66@gmail.com>
2024-11-28 12:34:05 +0100

    header con logo e items.center

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className='w-full flex justify-around bg-black text-white p-8'>
-        <Link href={'https://web.dragonball-api.com/'}><Title text='DRAGON BALL' /></Link>
+    <header className='w-full flex items-center justify-around bg-black text-white p-8'>
+        <Link href={'https://web.dragonball-api.com/'}><img src='/dragon-balls.webp' alt='logo api dragon ball' className='w-16'/></Link>
         <Link href={'/characters'}><Title text='Personajes' /></Link>
         <Link href={'https://www.linkedin.com/in/manuel-lev%C3%AD-torres-vicente/'}><Title text='MLEVITORRESV' /></Link>
     </header>
