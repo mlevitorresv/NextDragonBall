9c073875328d1506700afa8aafde59f0577f75be
mlevitorresv <levitorres66@gmail.com>
2024-11-28 11:16:51 +0100

    key añadido a parrafo

--- src/components/paragraphAvg.tsx
+++ src/components/paragraphAvg.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function ParagraphAvg(props: ParagraphAvgInterface) {
   return (
-    props.texts.map((text) => (
-        <p className='text-black w-3/5 mx-auto my-8'>
+    props.texts.map((text, index) => (
+        <p className='text-black w-3/5 mx-auto my-8' key={index}>
             {text}
         </p>
     )
