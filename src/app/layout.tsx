056c782eff47bbce1442e191f123577d8a099a7c
mlevitorresv <levitorres66@gmail.com>
2024-11-28 12:36:20 +0100

    kaushan añadida  <head>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+        <link href="https://fonts.googleapis.com/css2?family=Esteban&family=Kaushan+Script&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-orange-500`}
       >
