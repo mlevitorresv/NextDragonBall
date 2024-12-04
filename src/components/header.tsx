7011c531f58634460713c03f544e5af5e7b21df7
mlevitorresv <levitorres66@gmail.com>
2024-12-04 14:42:01 +0100

    title componente eliminado

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Title from '@/components/title'
 import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className='w-full flex items-center justify-around bg-black text-white p-8'>
         <Link href={'https://web.dragonball-api.com/'}><img src='/dragon-balls.webp' alt='logo api dragon ball' width='68' height='68'/></Link>
-        <Link href={'/characters'}><Title text='Personajes' /></Link>
+        {/* <Link href={'/characters'}><Title text='Personajes' /></Link> */}
         <Link href={'https://www.linkedin.com/in/manuel-lev%C3%AD-torres-vicente/'}><Title text='MLEVITORRESV' /></Link>
     </header>
   )
